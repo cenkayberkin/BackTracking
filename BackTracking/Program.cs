130bb82c36512893f555e9a13a75c18e9a713681
Mehmet Cenk Ayberkin <cenkayberkin@gmail.com>
2015-07-01 15:49:39 +0300

    Subset and Permutations solved

---
+++ BackTracking/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BackTracking
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+//			Subset s = new Subset (3);
+			List<int> list = new List<int>(){1,2,3};
+			Permutation p = new Permutation(list);
+
+
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ BackTracking/Subset.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BackTracking
+{
+	public class Subset
+	{
+		bool finished;
+		List<string> results;
+		public Subset (int n)
+		{
+			finished = false;
+			int[] A = new int[100];
+			results = new List<string> ();
+			SolveSubset (A,0,n);
+		}
+
+		void SolveSubset(int[] a,int k, int input)
+		{
+			int[] c= new int[100];
+			int nCandidates;
+
+			if (is_a_solution (a, k, input)) {
+				process_solution (a, k);
+			} else {
+				k = k + 1;
+				construct_candidates (a,k,input,c,out nCandidates);
+				for (int i = 0; i < nCandidates; i++) {
+					a[k] = c[i];
+					SolveSubset (a,k,input);
+					if (finished) {
+						return;
+					}
+				}
+			}
+		}
+
+		void construct_candidates(int[] a,int k,int input,int[] c,out int nCandidates)
+		{
+			c [0] = 0;
+			c [1] = 1;
+			nCandidates = 2;
+		}
+
+		bool is_a_solution(int[] a, int k, int n)
+		{
+			return (k == n);
+		}
+
+		void process_solution(int[] a, int k)
+		{
+			string s = string.Empty;
+			for (int i = 1; i < k + 1; i++) {
+				if (a[i] == 1) {
+					s += i.ToString();
+				}
+			}
+			results.Add (s);
+		}
+	}
+}
+
